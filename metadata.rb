2ba713328c3ab35fbcf7509c2fc9526f3d784cb9
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-05-27 16:45:06 +0400

    fixed template

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,6 @@ version "0.1.0"
 description "Unicorn config generator. Part of chef-rails-stack"
 maintainer "Oleg Bovykin"
 maintainer_email "oleg.bovykin@gmail.com"
+
+recipe "unicorn", "basic config for unicorn"
+recipe "unicorn::monit", "monit part for unicorn"
